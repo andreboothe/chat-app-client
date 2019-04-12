fc5c85f4d5391669e3ba1a9d73a06b656f3a580b
Andre Boothe <ABoothe1@heart-nta.org>
2019-04-12 13:54:00 -0500

    Base Application

---
+++ src/config.js
@@ -0,0 +1,4 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/27d8d234-bfa4-407c-a3de-2158e64fd162/token";
+const instanceLocator = "v1:us1:27d8d234-bfa4-407c-a3de-2158e64fd162";
+
+export { tokenUrl, instanceLocator }
